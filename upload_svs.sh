0f43d516b62167e120faa40d6b543eded66b83d9
Seth Speaks <sspeaks610@gmail.com>
2022-05-27 13:55:47 -0700

    adjust pipeline

--- upload_svs.sh
+++ upload_svs.sh
@@ -7,6 +7,9 @@ mkdir "$HOME/static"
 mv test_files "$HOME/static"
 mv test.dzi "$HOME/static"
 
+az storage blob delete-batch --account-name svsdemo -s '$web' --pattern 'test_files/*' --delete-snapshots include
+az storage blob delete-batch --account-name svsdemo -s '$web' --pattern 'test.dzi' --delete-snapshots include
+
 az storage blob upload-batch --account-name svsdemo --source "$HOME/static" --destination '$web' --overwrite
 
 rm -rf "$HOME/static"
